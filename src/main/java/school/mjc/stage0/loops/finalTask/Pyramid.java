62295703cc614edfdf49340aaa12e4ffe8719d40
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-25 17:54:36 +0600

    fix: final task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,29 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int rows = 5, k = 0, count = 0, count1 = 0;
 
+        for (int i = 1; i <= cathetusLength; ++i) {
+            for (int space = 1; space <= cathetusLength/2 - i; ++space) {
+                System.out.print("");
+                ++count;
+            }
+
+            while (k != 2 * i - 1) {
+                if (count <= cathetusLength - 1) {
+                    System.out.print((i + k) + " ");
+                    ++count;
+                } else {
+                    ++count1;
+                    System.out.print((i + k - 2 * count1) + " ");
+                }
+
+                ++k;
+            }
+            count1 = count = k = 0;
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
